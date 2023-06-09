214a70272a0886c97dad58aae1a6a2c15d23911f
EC2 Default User <ec2-user@ip-172-31-21-242.ap-northeast-1.compute.internal>
2023-06-09 17:42:42 +0000

    5

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about"=>"homes#about"
   get "search" => "searches#search"
+  get '/search', to: 'searches#search'
   
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
   resources :book_comments, only: [:create, :destroy]
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
