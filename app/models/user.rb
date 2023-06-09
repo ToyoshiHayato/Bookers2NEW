c518d820fe67b730e04e31f033ea632b7f2d802d
EC2 Default User <ec2-user@ip-172-31-21-242.ap-northeast-1.compute.internal>
2023-06-09 19:14:32 +0000

    5 Finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,11 +39,11 @@ end
     if method == 'perfect'
       User.where(name: content)
     elsif method == 'forward'
-      User.where('name LIKE?', content + '%')
+      User.where('name LIKE ?', content + '%')
     elsif method == 'backward'
-      User.where('name LIKE?', '%' + content)
+      User.where('name LIKE ?', '%' + content)
     else
-      User.where('name LIKE?', '%' + content + '%')
+      User.where('name LIKE ?', '%' + content + '%')
     end
  end
 end
